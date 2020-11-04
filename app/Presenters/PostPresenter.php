04fe759079a48c27627165531c0223805ddd7179
Jan Straka <jan.straka@ibm.com>
2020-11-04 01:20:41 +0100

    nette-blog - day3

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -24,5 +24,42 @@ class PostPresenter extends Nette\Application\UI\Presenter
         }
 
         $this->template->post = $post;
+        $this->template->comments = $post->related('comment')->order('created_at');
     }
+
+    protected function createComponentCommentForm(): Form
+    {
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('name', 'Jméno:')
+            ->setRequired();
+
+        $form->addEmail('email', 'E-mail:');
+
+        $form->addTextArea('content', 'Komentář:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Publikovat komentář');
+
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+
+        return $form;
+    }
+
+    public function commentFormSucceeded(Form $form, \stdClass $values): void
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert([
+            'post_id' => $postId,
+            'name' => $values->name,
+            'email' => $values->email,
+            'content' => $values->content,
+        ]);
+
+        $this->flashMessage('Děkuji za komentář', 'success');
+        $this->redirect('this');
+    }
+
+
 }
